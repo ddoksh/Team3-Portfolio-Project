accaf82eaf6f07055d93e99044f3b7f476f1a7e5
ddoksh <33309281+ddoksh@users.noreply.github.com>
2017-11-03 11:35:32 +0900

    changed rotation box

---
+++ Assets/RotationBox.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationBox : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Rotate(45 * Time.deltaTime, 15, 0);
+    }
+}
